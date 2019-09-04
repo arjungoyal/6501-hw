f46a3c546527925b98773c2dd3cd068b1d18562d
Hardis, Richard Phillip <rhardis3@gatech.edu>
2019-09-04 12:48:23 -0400

    Working code
    Finished the kknn and ksvm functions.  The final get_kfolds_expected_accuracy functiona also works and can be applied to the homework problems now.

--- HW2/HW2RCODE.R
+++ HW2/HW2RCODE.R
@@ -3,62 +3,99 @@
 library(kernlab)
 library(ggplot2)
 library(kknn)
-setwd("C:\Users\richa\Documents\GitHub\6501-hw\HW2")  # Change this for your local machine
+
+setwd("C:/Users/richa/Documents/GitHub/6501-hw/HW2")  # Change this for your local machine
 data_df = read.table("credit_card_data-headers.txt", header = TRUE)
-data = as.matrix(data_df)
+#data = as.matrix(data_df)
 
-# Plot best factor against all other factors
-model <- ksvm(data[,1:10],data[,11],type="C-svc", kernel="vanilladot",C=100,scaled=TRUE)
-a = colSums(model@xmatrix[[1]] * model@coef[[1]])
-a0 = -model@b
-
-col_list = colnames(data_df[,-11])
-low_factors = col_list[col_list != "A9"]
-low_factors
-
-accA9 = sum(data_df$R1 * data_df$A9) / nrow(data_df) * 100
-
-data_df$R1 = as.factor(data_df$R1)
-ggplot(data_df, aes(x=A9, y=A2, shape=R1, color=R1))+
-  geom_point()
-
-
-
-# KNN
-kknn_accuracy = function(k_val){
-  pred_responses <- rep(0, nrow(cred_card_data_headers)) #vector of 0s to insert the predicted response value of the model.
-  for (i in 1:nrow(cred_card_data_headers)){
-    #using [-i] to exclude the ith data point from the nearest neighbor calculation
-    kknn.model = kknn(R1 ~., train = cred_card_data_headers[-i,], test = cred_card_data_headers[i,], k = k_val, kernel = "rectangular",scale = TRUE)
-    #fitted(kknn.model) gives the value of the fraction of nearest neighbors to the ith data point that are 1.
-    fit <- fitted(kknn.model)
-    binary_response <- as.integer(fit + 0.5) #maps the fitted value to 0 or 1
-    pred_responses[i] <- binary_response #adds the model's fitted value to the pred_responses vector
-  }
-  acc = sum(pred_responses == cred_card_data_headers[,11]) / nrow(cred_card_data_headers)
+# KSVM
+ksvm_accuracy = function(train_data, test_data, kern, C){
+  model = ksvm(train_data[,1:10],train_data[,11], type="C-svc", kernel=kern, C=C, scaled=TRUE)
+  pred = predict(model,test_data[,1:10])
+  acc = sum(pred == test_data[,11]) / nrow(test_data)
   return(acc)
 }
 
-k_acc <- rep(0, 20)
-for (i in 1:20){ #values of k
-  k_acc[i] = kknn_accuracy(i)
+# KNN
+kknn_accuracy = function(train_data, k_val, kern){
+  pred_responses <- rep(0, nrow(train_data)) #vector of 0s to insert the predicted response value of the model.
+  for (i in 1:nrow(train_data)){
+    #using [-i] to exclude the ith data point from the nearest neighbor calculation
+    kknn.model = kknn(R1 ~., train = train_data[-i,], test = train_data[i,], k = k_val, kernel = kern, scale = TRUE)
+    #fitted(kknn.model) gives the value of the fraction of nearest neighbors to the ith data point that are 1.
+    fit = fitted(kknn.model)
+    binary_response <- as.integer(fit + 0.5) #maps the fitted value to 0 or 1
+    pred_responses[i] <- binary_response #adds the model's fitted value to the pred_responses vector
+  }
+  acc = sum(pred_responses == train_data[,11]) / nrow(train_data)
+  return(acc)
 }
 
-
-max(k_acc) #best k accuracy
-
-max_k <- which(k_acc %in% max(k_acc))
-max_k
-
-ggplot(as.data.frame(k_acc), aes(x= 1:20, y=k_acc)) + geom_point() +
-  ggtitle("Accuracies of K-Nearest Neighbors Model by Values of K") +
-  xlab("Integer Values of K") + ylab("Accuracy")
-
-"'
-Write the functions below:
-'"
-
 # Function for K-folds.  Takes in dataset, model type, model parameters (parameters needs to be flexible depending on SVM, KNN or others)
+get_kfolds_expected_accuracy = function(data_df, k_folds, MODEL_TYPE){
+  "
+  Arguments:
+    data_array: data dataframe of the dataset to run k-folds cross validation with
+    k_folds: the number of folds to use in k-folds cross validation
+    model_function: the name of the modeling function.  Either 'ksvm' or 'kknn' for this HW.
+  
+  Return:
+    expected_accuracy: the average accuracy of the models in the k-folds number of training-testing splits
+  "
+  
+  # Get the k folds data groups
+  number_test_points = floor(nrow(data_df)/k_folds)
+  data_copy = data_df
+  kf_groups = list()
+  
+  for (i in 1:k_folds){
+    if (i==k_folds){kf_groups[[i]] = data_copy}
+    else{
+      set_list = train_test_split(data_copy, 0, number_test_points)
+      new_test_group = set_list[[2]]
+      data_copy = set_list[[1]]
+      kf_groups[[i]] = new_test_group
+    }
+  }
+  
+  # Blank accuracy list
+  accuracies = list()
+  
+  # Get the accuracy performance of each of the k models
+  for (i in 1:k_folds){
+    # Get the test group of data and the training group of data
+    test_df = kf_groups[[i]]
+    train_groups = kf_groups[-i]
+    training_df = train_groups[[1]]
+    
+    for (group in train_groups[-1]){
+      training_df = rbind(training_df, group)
+    }
+
+    # Transform test and train dataframes into matrices
+    test = test_df
+    train = training_df
+    
+    # Train the model on the training data
+    if (MODEL_TYPE=='ksvm'){    # KSVM
+      train_mat = as.matrix(train)
+      test_mat = as.matrix(test)
+      current_accuracy = ksvm_accuracy(train_mat, test_mat, "rbfdot", 100)
+    }
+    else{    # KKNN
+      current_accuracy = kknn_accuracy(train, 6, "rectangular")
+    }
+
+    # Put the test accuracy into the accuracy list
+    accuracies[[i]] = current_accuracy
+  }
+  
+  # Find the average of the accuracy list
+  expected_accuracy = mean(unlist(accuracies))
+  
+  # Return the expected accuracy
+  return(expected_accuracy)
+}
 
 # Function for Splitting data randomly into training and testing data.  Returns two dataframes or arrays?
 train_test_split = function(data, test_percent=0, num_test_points=-1){
@@ -79,7 +116,9 @@ train_test_split = function(data, test_percent=0, num_test_points=-1){
   test_indices = sample(1:num_observations, num_test_points, replace=FALSE)
   test_set = data[test_indices,]
   training_set = data[-test_indices,]    # -test_indices removes all the rows with the test indices
-  
-  return(c(training_set,test_set))
+
+  return(list(training_set,test_set))
 }
 
+# Get the cross-validated accuracy of a certain model with a given dataset
+expected_accuracy_over_kfolds = get_kfolds_expected_accuracy(data_df, 10, 'kknn')
