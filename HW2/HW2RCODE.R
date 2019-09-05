559f5f8614164b3e17f61bda141b895463337753
Hardis, Richard Phillip <rhardis3@gatech.edu>
2019-09-05 16:46:03 -0400

    3.1 and 4.1
    3.1 and 4.1

--- HW2/HW2RCODE.R
+++ HW2/HW2RCODE.R
@@ -38,16 +38,16 @@ get_kfolds_expected_accuracy = function(data_df, k_folds, MODEL_TYPE, MODEL_PARA
     data_array: data dataframe of the dataset to run k-folds cross validation with
     k_folds: the number of folds to use in k-folds cross validation
     model_function: the name of the modeling function.  Either 'ksvm' or 'kknn' for this HW.
-  
+
   Return:
     expected_accuracy: the average accuracy of the models in the k-folds number of training-testing splits
   "
-  
+
   # Get the k folds data groups
   number_test_points = floor(nrow(data_df)/k_folds)
   data_copy = data_df
   kf_groups = list()
-  
+
   for (i in 1:k_folds){
     if (i==k_folds){kf_groups[[i]] = data_copy}
     else{
@@ -57,17 +57,17 @@ get_kfolds_expected_accuracy = function(data_df, k_folds, MODEL_TYPE, MODEL_PARA
       kf_groups[[i]] = new_test_group
     }
   }
-  
+
   # Blank accuracy list
   accuracies = list()
-  
+
   # Get the accuracy performance of each of the k models
   for (i in 1:k_folds){
     # Get the test group of data and the training group of data
     test_df = kf_groups[[i]]
     train_groups = kf_groups[-i]
     training_df = train_groups[[1]]
-    
+
     for (group in train_groups[-1]){
       training_df = rbind(training_df, group)
     }
@@ -75,7 +75,7 @@ get_kfolds_expected_accuracy = function(data_df, k_folds, MODEL_TYPE, MODEL_PARA
     # Transform test and train dataframes into matrices
     test = test_df
     train = training_df
-    
+
     # Train the model on the training data
     if (MODEL_TYPE=='ksvm'){    # KSVM
       train_mat = as.matrix(train)
@@ -89,10 +89,10 @@ get_kfolds_expected_accuracy = function(data_df, k_folds, MODEL_TYPE, MODEL_PARA
     # Put the test accuracy into the accuracy list
     accuracies[[i]] = current_accuracy
   }
-  
+
   # Find the average of the accuracy list
   expected_accuracy = mean(unlist(accuracies))
-  
+
   # Return the expected accuracy
   return(expected_accuracy)
 }
@@ -109,11 +109,11 @@ train_test_split = function(data, test_percent=0, num_test_points=-1){
   "
   num_observations = nrow(data) # How many data points are there?
   print(num_observations)
-  
+
   if(num_test_points==-1){    # The user has requested to use a specific number of points for testing
     num_test_points = floor(num_observations*(test_percent/100))
   }
-  
+
   test_indices = sample(1:num_observations, num_test_points, replace=FALSE)
   test_set = data[test_indices,]
   training_set = data[-test_indices,]    # -test_indices removes all the rows with the test indices
@@ -174,9 +174,7 @@ print(test_knn_acc)
 ##########################
 library(datasets)
 library(ggplot2)
-#summary(iris)
-#ggplot(iris,  aes(Petal.Length, Petal.Width, color=Species))+geom_point()
-#ggplot(iris, aes(Sepal.Length, Sepal.Width, color=Species))+geom_point()
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color=Species))+geom_point()
 summary(iris)
 # Remove the species label from Iris dataset
 iris_no_class = iris[,-5]
